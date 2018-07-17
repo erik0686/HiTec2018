fe7f47e6f58d1a4d61c6a1db781f7f412efa7e52
Erik Torres <erik.torres.aguilar@gmail.com>
2018-07-16 21:07:11 -0500

    create staff

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -7,4 +7,28 @@ class StaffsController < ApplicationController
 
   end
 
+  def new
+    @staff = Staff.new
+  end
+
+  def create
+    @staff = Staff.new(staff_params)
+    if @staff.save
+      redirect_to staff_path(id: @staff.id, matricula: @staff.matricula)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @staff = Staff.find(params[:id])
+    @matricula = params[:matricula]
+  end
+
+  private
+
+  def staff_params
+    params.require(:staff).permit(:matricula, :role, :building_id)
+  end
+
 end
