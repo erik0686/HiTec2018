b67fd9425e9246ead34969102723c1d0c3eb9cfd
Erik Torres <erik.torres.aguilar@gmail.com>
2018-07-23 23:37:24 -0500

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    user_session_path
+    return redirect_to user_session_path
   end
 end
